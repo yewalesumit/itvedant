2f561f5326df0a18788be1e5472c944ad3166812
yewalesumit <sumityewale498@gmail.com>
2025-07-25 11:54:10 +0530

    infomedia project

---
+++ Javascript/23th_july/hello.js
@@ -0,0 +1,8 @@
+alert("Hello World!");
+
+var name1 = "Pratik";
+var name2 = "Nawale";
+
+console.log(name1 +" "+ name2);
+
+
---
+++ Javascript/23th_july/variable.js
@@ -0,0 +1,37 @@
+//variable Declaration 
+
+//var
+// var user = "Sumit";
+// console.log(user)
+
+// var user = "Jaydip"
+// console.log(user)
+
+//let
+// let user = "Sumit";
+// console.log(user)
+
+// let user = "Jaydip"
+// console.log(user)
+
+//const
+// const user = "Sumit";
+// console.log(user)
+
+// const user = "Jaydip"
+// console.log(user)
+
+
+//Variable Hoisting 
+
+//var
+console.log(x);
+var x = 20;
+
+//let
+console.log(y);
+let y = 20;
+
+//const
+console.log(z);
+const z = 20;
