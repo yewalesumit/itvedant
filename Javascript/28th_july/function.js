c5e0120a2bb4057dc4953714a4b8d1ccd99c775a
yewalesumit <sumityewale498@gmail.com>
2025-07-28 14:39:19 +0530

    Summery of 28th_july

---
+++ Javascript/28th_july/function.js
@@ -0,0 +1,79 @@
+//User define function 
+
+// function Greet(name){
+//     console.log(`hello,${name}!`);
+// }
+
+// let name = prompt("Enter your name:")
+// Greet(name);
+
+// alert("Danger:::")
+
+// let st = "Acd"
+
+// console.log(st.toUpperCase())
+// console.log(st.toLowerCase())
+
+
+// let arr = [1,2,3,4,5,6];
+
+// arr.push(7);
+// arr.unshift(0);
+// arr.pop()
+// console.log(arr);/
+
+
+// let arr = [1,2,3,4,5,6];
+
+// console.log(Math.max(...arr))
+
+// let date = new Date();
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDate());
+
+//Callback Function
+
+// function Sum(a , b){
+//     return(a+b);
+    
+// }
+
+// function Calculator(func, x, y){
+//     let add = func(x,y);
+//     console.log(add);
+
+// }
+
+// Calculator(Sum, 10, 20);
+
+//Arrow functions
+
+// let sayHello = (name) => console.log(`Hello,${name}!`)
+
+
+// sayHello("nawale");
+
+// function Greet(name = 'user'){
+//     console.log(`Hello,${name}`)
+// }
+
+// Greet();
+
+
+//anonymous function
+
+// (function() {
+//     console.log('Hello Everyone this is ItVedant')
+// })
+// ();
+
+let a = 12.687945;
+
+console.log(Math.round(a));
+console.log(Math.floor(a));
+console.log(Math.ceil(a));
+console.log(a.toFixed());
+console.log(a.toPrecision());
+
+
